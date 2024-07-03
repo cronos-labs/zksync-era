786ba3e4255e029a34607755de1abc051e7910ab
Ivan SL Wong <ivan.sl.wong@crypto.com>
2024-07-03 17:49:23 +0800

    Add external node

--- flake.nix
+++ flake.nix
@@ -3,12 +3,14 @@
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
   inputs.std.url = "github:divnix/std";
 
+  inputs.ml-zksync-24-3-0.url = "github:matter-labs/zksync-era/core-v24.3.0";
   inputs.zkevm-23-0-0.url = "github:cronos-labs/cronos-zkevm/testnet-23.0.0";
   inputs.zkevm-24-0-0.url = "github:cronos-labs/cronos-zkevm/testnet-v24.0.0";
   inputs.zkevm-24-0-0-validium-fix.url = "github:cronos-labs/cronos-zkevm/testnet-v24.0.0-validium-fix";
   inputs.zkevm-24-2-0.url = "github:cronos-labs/cronos-zkevm/testnet-v24.2.0";
   inputs.zkevm-24-2-0-add-log-seal-criteria.url = "github:cronos-labs/cronos-zkevm/thomas/add-log-seal-criteria";
 
+  inputs.ml-zksync-24-3-0.flake = false;
   inputs.zkevm-23-0-0.flake = false;
   inputs.zkevm-24-0-0.flake = false;
   inputs.zkevm-24-0-0-validium-fix.flake = false;
@@ -24,10 +26,11 @@
       inherit inputs;
       cellsFrom = ./nix;
       cellBlocks = with std.blockTypes; [
+        (installables "en" {ci.build = false;})
         (installables "prover" {ci.build = false;})
         (installables "server" {ci.build = false;})
       ];
     } {
-      packages = std.harvest self [["local" "prover"] ["local" "server"]];
+      packages = std.harvest self [["local" "en"] ["local" "prover"] ["local" "server"]];
     };
 }
