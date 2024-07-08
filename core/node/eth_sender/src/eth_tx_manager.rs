c99e25f1698bb9a4c02d237d4d1bd52b726b459c
Thomas Nguy <thomas.nguy@crypto.com>
2024-07-09 00:27:51 +0900

    cap priority fee per gas

--- core/node/eth_sender/src/eth_tx_manager.rs
+++ core/node/eth_sender/src/eth_tx_manager.rs
@@ -19,6 +19,7 @@ use zksync_types::{
 use zksync_utils::time::seconds_since_epoch;
 
 use super::{metrics::METRICS, ETHSenderError};
+use crate::ETHSenderError::ExceedMaxBaseFee;
 
 #[derive(Debug)]
 struct EthFee {
@@ -244,17 +245,17 @@ impl EthTxManager {
             blob_base_fee_per_gas,
         } = self.calculate_fee(storage, tx, time_in_mempool).await?;
 
-        let mut base_fee_per_gas_cap = base_fee_per_gas;
-        if base_fee_per_gas_cap > self.config.max_acceptable_base_fee_in_wei {
-            base_fee_per_gas_cap = self.config.max_acceptable_base_fee_in_wei;
-            tracing::debug!(
-                "initial base_fee {}, max reached and cap to {}",
-                base_fee_per_gas,
-                base_fee_per_gas_cap
+        if base_fee_per_gas > self.config.max_acceptable_base_fee_in_wei {
+            tracing::info!(
+                    "base fee per gas: {} exceed max acceptable fee in configuration: {}, skip transaction",
+                    base_fee_per_gas,
+                    self.config.max_acceptable_base_fee_in_wei
             );
+
+            return Err(ExceedMaxBaseFee);
         }
 
-        METRICS.used_base_fee_per_gas.observe(base_fee_per_gas_cap);
+        METRICS.used_base_fee_per_gas.observe(base_fee_per_gas);
         METRICS
             .used_priority_fee_per_gas
             .observe(priority_fee_per_gas);
@@ -270,12 +271,7 @@ impl EthTxManager {
         };
 
         let mut signed_tx = self
-            .sign_tx(
-                tx,
-                base_fee_per_gas_cap,
-                priority_fee_per_gas,
-                blob_gas_price,
-            )
+            .sign_tx(tx, base_fee_per_gas, priority_fee_per_gas, blob_gas_price)
             .await;
 
         if let Some(blob_sidecar) = &tx.blob_sidecar {
@@ -289,7 +285,7 @@ impl EthTxManager {
             .eth_sender_dal()
             .insert_tx_history(
                 tx.id,
-                base_fee_per_gas_cap,
+                base_fee_per_gas,
                 priority_fee_per_gas,
                 blob_base_fee_per_gas,
                 signed_tx.hash,
@@ -305,7 +301,7 @@ impl EthTxManager {
                 tracing::warn!(
                     "Error when sending new signed tx for tx {}, base_fee_per_gas {}, priority_fee_per_gas: {}: {}",
                     tx.id,
-                    base_fee_per_gas_cap,
+                    base_fee_per_gas,
                     priority_fee_per_gas,
                     error
                 );
