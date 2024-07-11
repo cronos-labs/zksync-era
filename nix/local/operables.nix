270b984dd7b8c5c49fef40c301b20421b38bf299
Ivan SL Wong <ivan.sl.wong@crypto.com>
2024-07-11 14:52:28 +0800

    Fix operables packages

--- nix/local/operables.nix
+++ nix/local/operables.nix
@@ -3,7 +3,7 @@
     inputs.std.lib.ops.mkOperable rec
     {
       runtimeInputs = [inputs.nixpkgs.bashInteractive inputs.nixpkgs.coreutils];
-      package = cell.en.external-node;
+      package = cell.en.en.external_node;
       runtimeScript = ''
         exec ${package}/bin/zksync_external_node "$@"
       '';
