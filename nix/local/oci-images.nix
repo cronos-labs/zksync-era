5dd99a7dcc9cc0d0f20281c42235bf4a4cd64241
Henry Wong <132334382+henrywong-crypto@users.noreply.github.com>
2024-08-02 12:09:43 +0800

    Update oci-images.nix

--- nix/local/oci-images.nix
+++ nix/local/oci-images.nix
@@ -3,7 +3,7 @@ let
   base-image = inputs.n2c.packages.nix2container.pullImage {
     imageName = "ghcr.io/cronos-labs/zkevm-base-image";
     imageDigest = "sha256:aeaa2825da75b00fbd63e5f7f9dbd825098b1b068ed7397a479e9860b077af42";
-    sha256 = "";
+    sha256 = "sha256-9XuuqBBgNcRXkB4iJ9oSYzx5wUaMXgVIj602uDvPdcQ=";
   };
   db = inputs.nixpkgs.runCommand "db" {} ''
     mkdir -p $out/db
