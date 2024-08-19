3983c90f1f49fc73472a9ebd29ecaac667614bb2
Henry Wong <henry.hs.wong@crypto.com>
2024-08-19 12:36:52 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -1,70 +1,64 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.n2c.url = "github:nlewo/nix2container";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
-  inputs.std.url = "github:divnix/std";
 
-  inputs.std.inputs.n2c.url = "github:nlewo/nix2container";
+  inputs.zksync-era.url = "github:matter-labs/zksync-era/core-v24.9.0";
 
-  inputs.ml-zksync-24-3-0.url = "github:matter-labs/zksync-era/core-v24.3.0";
-  inputs.ml-zksync-24-6-0.url = "github:matter-labs/zksync-era/core-v24.6.0";
-  inputs.ml-zksync-24-9-0.url = "github:matter-labs/zksync-era/core-v24.9.0";
-  inputs.zkevm-23-0-0.url = "github:cronos-labs/cronos-zkevm/testnet-23.0.0";
-  inputs.zkevm-24-0-0.url = "github:cronos-labs/cronos-zkevm/testnet-v24.0.0";
-  inputs.zkevm-24-0-0-validium-fix.url = "github:cronos-labs/cronos-zkevm/testnet-v24.0.0-validium-fix";
-  inputs.zkevm-24-2-0.url = "github:cronos-labs/cronos-zkevm/testnet-v24.2.0";
-  inputs.zkevm-24-9-0-testnet.url = "github:cronos-labs/cronos-zkevm/testnet-v24.9.0";
-
-  inputs.ml-zksync-24-3-0.flake = false;
-  inputs.ml-zksync-24-6-0.flake = false;
-  inputs.ml-zksync-24-9-0.flake = false;
-  inputs.zkevm-23-0-0.flake = false;
-  inputs.zkevm-24-0-0.flake = false;
-  inputs.zkevm-24-0-0-validium-fix.flake = false;
-  inputs.zkevm-24-2-0.flake = false;
+  inputs.zksync-era.flake = false;
 
   outputs = {
     flake-utils,
+    nixpkgs,
+    rust-overlay,
     self,
-    std,
     ...
   } @ inputs:
-    std.growOn {
-      inherit inputs;
-      cellsFrom = ./nix;
-      cellBlocks = with std.blockTypes; [
-        (containers "oci-images" {ci.publish = true;})
-        (installables "en" {ci.build = false;})
-        (installables "prover" {ci.build = false;})
-        (installables "server" {ci.build = false;})
-        (installables "packages")
-        (runnables "operables")
-      ];
-    } {
-      packages = std.harvest self [["local" "en"] ["local" "prover"] ["local" "server"] ["local" "packages"]];
-    } (
-      flake-utils.lib.eachDefaultSystem (system: let
-        pkgs = import inputs.nixpkgs {
-          crossSystem = "aarch64-linux";
-          inherit system;
-          overlays = [(import inputs.rust-overlay)];
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+    in
+      with pkgs; let
+        rustPlatform' = makeRustPlatform {
+          cargo = rust-bin.fromRustupToolchainFile (inputs.zksync-era + /rust-toolchain);
+          rustc = rust-bin.fromRustupToolchainFile (inputs.zksync-era + /rust-toolchain);
         };
-        en = import ./nix/local/en2.nix {inherit pkgs inputs;};
-      in
-        with pkgs; {
-          packages.docker-aarch64-mainnet = dockerTools.buildImage {
-            name = "ghci.io/cronos-labs/external-node";
-            tag = "v24.9.0-mainnet";
-            copyToRoot = buildEnv {
-              name = "image-root";
-              paths = [dockerTools.caCertificates bashInteractive coreutils en.en.external_node];
-            };
-            extraCommands = ''
-              mkdir -p /db
-              chmod 777 /db
-            '';
+        base-image = dockerTools.pullImage {
+          finalImageTag = "mainnet-v24.9.0";
+          imageDigest = "sha256:aeaa2825da75b00fbd63e5f7f9dbd825098b1b068ed7397a479e9860b077af42";
+          imageName = "ghcr.io/cronos-labs/zkevm-base-image";
+          sha256 = "sha256-9XuuqBBgNcRXkB4iJ9oSYzx5wUaMXgVIj602uDvPdcQ=";
+        };
+        external-node = rustPlatform'.buildRustPackage.override {stdenv = clangStdenv;} {
+          buildInputs = [openssl];
+          cargoBuildFlags = "--bin zksync_external_node";
+          cargoHash = "sha256-7CO48+RFqlhm+/6QVNyX8059orU9DhrzLbZyEA/M0hg=";
+          doCheck = false;
+          nativeBuildInputs = [pkg-config rustPlatform.bindgenHook];
+          pname = "external-node";
+          src = inputs.zksync-era + /.;
+          version = "dummy";
+        };
+      in {
+        packages.mainnet = dockerTools.buildImage {
+          name = "mainnet";
+          tag = "nix";
+          fromImage = mainnet;
+          copyToRoot = buildEnv {
+            name = "image-root";
+            paths = [
+              bashInteractive
+              coreutils
+              dockerTools.caCertificates
+            ];
           };
-        })
-    );
+          config.Cmd = pkgs.writeScript "cmd" ''
+            #!${bash}/bin/bash
+            ${sqlx-cli}/bin/sqlx database setup
+            exec ${external-node}/bin/zksync_external_node "$@"
+          '';
+        };
+      });
 }
