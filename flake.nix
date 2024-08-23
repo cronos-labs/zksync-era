910b6632424125b283b798a3cf033cdaba524883
Henry Wong <henry.hs.wong@crypto.com>
2024-08-23 10:22:01 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -25,17 +25,21 @@
           cargo = rust-bin.fromRustupToolchainFile (inputs.zksync-era + /rust-toolchain);
           rustc = rust-bin.fromRustupToolchainFile (inputs.zksync-era + /rust-toolchain);
         };
-        base-image-mainnet =
-          (dockerTools.override
-            {
-              skopeo = pkgs.writeScriptBin "skopeo" ''exec ${skopeo}/bin/skopeo "$@" --authfile=/etc/docker/config.json'';
-            })
-          .pullImage {
-            finalImageTag = "mainnet-v24.9.0";
-            imageDigest = "sha256:aeaa2825da75b00fbd63e5f7f9dbd825098b1b068ed7397a479e9860b077af42";
-            imageName = "ghcr.io/cronos-labs/zkevm-base-image";
-            sha256 = "sha256-GQGaojsWBf0QNRSAj6vQAS+KElIXIRIBQxaxLEszpEs=";
-          };
+        dockerTools' = dockerTools.override {
+          skopeo = pkgs.writeScriptBin "skopeo" ''exec ${skopeo}/bin/skopeo "$@" --authfile=/etc/docker/config.json'';
+        };
+        base-image-mainnet = dockerTools'.pullImage {
+          finalImageTag = "mainnet-v24.9.0";
+          imageDigest = "sha256:aeaa2825da75b00fbd63e5f7f9dbd825098b1b068ed7397a479e9860b077af42";
+          imageName = "ghcr.io/cronos-labs/zkevm-base-image";
+          sha256 = "sha256-GQGaojsWBf0QNRSAj6vQAS+KElIXIRIBQxaxLEszpEs=";
+        };
+        base-image-testnet = dockerTools'.pullImage {
+          finalImageTag = "testnet-v24.9.0";
+          imageDigest = "sha256:28a7022cda8e5aa6abffd296213b9d01261846a9557d66f86dcdf6720600dbec";
+          imageName = "ghcr.io/cronos-labs/zkevm-base-image";
+          sha256 = "";
+        };
         external-node = rustPlatform'.buildRustPackage.override {stdenv = clangStdenv;} {
           buildInputs = [openssl];
           cargoBuildFlags = "--bin zksync_external_node";
@@ -51,6 +55,24 @@
           name = "mainnet";
           tag = "nix";
           fromImage = base-image-mainnet;
+          copyToRoot = buildEnv {
+            name = "image-root";
+            paths = [
+              bashInteractive
+              coreutils
+              dockerTools.caCertificates
+            ];
+          };
+          config.Cmd = pkgs.writeScript "cmd" ''
+            #!${bash}/bin/bash
+            ${sqlx-cli}/bin/sqlx database setup
+            exec ${external-node}/bin/zksync_external_node "$@"
+          '';
+        };
+        packages.testnet = dockerTools.buildImage {
+          name = "testnet";
+          tag = "nix";
+          fromImage = base-image-testnet;
           copyToRoot = buildEnv {
             name = "image-root";
             paths = [
