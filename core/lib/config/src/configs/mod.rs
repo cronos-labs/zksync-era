082af609ac4d7a263c5cfed227ba7446af2d3f2c
JayT106 <JayT106@users.noreply.github.com>
2024-10-25 19:39:35 -0400

    feat: missing DenyList feature (#92)

--- core/lib/config/src/configs/mod.rs
+++ core/lib/config/src/configs/mod.rs
@@ -28,6 +28,7 @@ pub use self::{
     secrets::{DatabaseSecrets, L1Secrets, Secrets},
     snapshot_recovery::SnapshotRecoveryConfig,
     snapshots_creator::SnapshotsCreatorConfig,
+    tx_sink::TxSinkConfig,
     utils::PrometheusConfig,
     vm_runner::{BasicWitnessInputProducerConfig, ProtectiveReadsWriterConfig},
 };
@@ -66,6 +67,7 @@ pub mod pruning;
 pub mod secrets;
 pub mod snapshot_recovery;
 pub mod snapshots_creator;
+pub mod tx_sink;
 pub mod utils;
 pub mod vm_runner;
 pub mod wallets;
--- core/lib/protobuf_config/src/lib.rs
+++ core/lib/protobuf_config/src/lib.rs
@@ -36,6 +36,7 @@ mod snapshot_recovery;
 mod snapshots_creator;
 #[cfg(test)]
 mod tests;
+mod tx_sink;
 mod utils;
 mod vm_runner;
 mod wallets;
