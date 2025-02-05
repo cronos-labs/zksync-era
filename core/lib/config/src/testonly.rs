2da9c0969e522e4bc6d0936a827ef6849367a31d
jay.tseng <jaytseng106@gmail.com>
2025-02-05 11:04:56 -0500

    feat: missing GKMSSigner feature (#98)
            Co-authored-by: Calvin Lau <38898718+calvinaco@users.noreply.github.com>

--- core/lib/config/src/testonly.rs
+++ core/lib/config/src/testonly.rs
@@ -23,6 +23,7 @@ use crate::{
             avail::{AvailClientConfig, AvailDefaultConfig},
             DAClientConfig::Avail,
         },
+        eth_sender::SigningMode,
         external_price_api_client::ForcedPriceClientConfig,
     },
     AvailConfig,
@@ -420,6 +421,7 @@ impl Distribution<configs::eth_sender::SenderConfig> for EncodeDist {
             tx_aggregation_only_prove_and_execute: false,
             time_in_mempool_in_l1_blocks_cap: self.sample(rng),
             is_verifier_pre_fflonk: self.sample(rng),
+            signing_mode: SigningMode::PrivateKey,
         }
     }
 }
